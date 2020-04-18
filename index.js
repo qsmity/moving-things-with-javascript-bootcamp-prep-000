694b0f76cf99eb59ba73016eadffed6669a2c72c
alpha-acrobat-9971 <qsmity@gmail.com>
2020-04-18 22:19:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much."
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.left.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+ 
+  if (right > 0) {
+    dodger.style.left = `${right + 10}px`}}
+
